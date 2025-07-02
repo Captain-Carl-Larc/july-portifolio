54dce4faeb0127de4eaf08e20a46bca9df9fd04d
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-07-02 11:44:05 +0300

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/about";
 import Skills from "./components/skills";
 import Projects from "./components/projects";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Skills />
       <Projects />
+      <Footer />
     </>
   );
 }

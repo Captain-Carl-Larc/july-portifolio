d56378ae8ea4312f71c4db1a0fccbc808cd8a600
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-07-02 10:08:51 +0300

    hero section made

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,13 +6,23 @@ function Hero() {
     />
   );
   return (
-    <section className="bg-gray-800 h-96  md:h-screen">
-        <div className="text-container">
-            Text comes here
+    <section className="justify-center items-start gap-4 grid md:grid-cols-2 md:grid-rows-1 bg-gray-800 px-4 pt-24 md:pt-56 pb-16 md:pb-0 md:h-screen min-h-96">
+      <div className="md:mb-0 text-container md:text-left text-center mb:4">
+        <h1 className="py-2 font-bold text-white text-3xl md:text-5xl">
+          Hello, I'm <span className="text-blue-800">Stephen Kalwenzi</span>
+        </h1>
+        <h1 className="my-4 text-blue-600 text-2xl md:text-4xl">
+          A Full Stack Developer
+        </h1>
+          <button className="bg-blue-600 px-4 py-2 rounded-lg font-bold">
+            Get in Touch
+          </button>
+      </div>
+      <div id="visual-container" className="flex justify-center items-center">
+        <div className="shadow-lg rounded-xl max-w-md overflow-hidden hero-image-container">
+          {heroImage}
         </div>
-        <div className="image-cont">
-            IMage comes here
-        </div>
+      </div>
     </section>
   );
 }

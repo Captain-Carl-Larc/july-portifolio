6c31a2da29a8b676ef488bd47264c3a401c75bd7
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-07-03 14:09:34 +0300

    UI update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,7 +6,10 @@ function Hero() {
     />
   );
   return (
-    <section id="home" className="justify-center items-start md:items-center gap-2 grid md:grid-cols-2 md:grid-rows-1 bg-blue-800 px-4 pt-24 md:pt-32 pb-16 md:pb-0 md:h-screen min-h-96">
+    <section
+      id="home"
+      className="justify-center items-start md:items-center gap-2 grid md:grid-cols-2 md:grid-rows-1 bg-blue-800 px-4 pt-24 md:pt-32 pb-16 md:pb-0 md:h-screen min-h-96"
+    >
       <div className="md:mb-0 text-container md:text-left text-center mb:4">
         <h1 className="py-2 font-bold text-gray-200 text-3xl md:text-5xl">
           Hello, I'm <span className="text-gray-300">Stephen Kalwenzi</span>
@@ -14,9 +17,10 @@ function Hero() {
         <h1 className="my-4 text-gray-300 text-2xl md:text-4xl">
           A Full Stack Developer
         </h1>
-          <button className="bg-orange-400 hover:bg-orange-500 px-4 py-2 rounded-lg font-bold text-white transition duration-300 cursor-pointer">
-            Get in Touch
-          </button>
+        <p className="py-4">Building modern, responsive, and user-friendly web experiences</p>
+        <button className="bg-orange-400 hover:bg-orange-500 px-4 py-2 rounded-lg font-bold text-white transition duration-300 cursor-pointer">
+          Get in Touch
+        </button>
       </div>
       <div id="visual-container" className="flex justify-center items-center">
         <div className="shadow-lg rounded-xl max-w-md overflow-hidden hero-image-container">
